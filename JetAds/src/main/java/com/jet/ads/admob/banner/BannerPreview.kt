162bf7ae5cd3b40bf972963cf0f306bd854c6c3a
Vinícius Henrique da silva corrêa <8inicius@gmail.com>
2024-09-30 18:57:02 -0300

    fixed bug cant impot composables

--- JetAds/src/main/java/com/jet/ads/admob/banner/BannerPreview.kt
+++ JetAds/src/main/java/com/jet/ads/admob/banner/BannerPreview.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.app.R
+import com.jet.ads.R
 import com.jet.ads.admob.AdMobTestIds
 
 @Composable
